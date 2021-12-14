ce3e9b80f9f1edfb0a2ed1fd71b73b4ea9a149e1
Gurupriyan <gurupriyan98@gmail.com>
2021-12-14 15:56:19 +0000

    Finished the code challenge

--- index.js
+++ index.js
@@ -6,12 +6,13 @@ const alumni = [
 {name:'Shemar', job:'SquareSpace',language:'JavaScript', age:23},
 {name:'Cielo', job:'NYT',language:'JavaScript', age:21}];
 
+const animals =['cheetah','dog', 'cat', 'dodobird', 'bear', 'dolphin'];
 //1
 function averageAge (array) {
     return Math.floor(array.map(person => person.age).reduce((a,b) => a+b)/array.length);
     //return array.reduce((a, b) => return {a + b.age}, 0)/;
 }
-console.log(averageAge(alumni));
+// console.log(averageAge(alumni));
 
 //2
 function bananaBread (array) {
@@ -42,3 +43,35 @@ function allUseJavaScript (array) {
     return array.every(person => person.language === "JavaScript");
 }
 // console.log(allUseJavaScript(alumni));
+
+
+//6
+function noVowel (array) {
+    const vowels = /[aeiou]/gi;
+    return array.map(word => word.replace(vowels, "x"));
+}
+// console.log(noVowel(animals));
+
+//7
+function onlyVowelA (array) {
+    return array.filter(word => word.includes("a"));
+}
+// console.log(onlyVowelA(animals));
+
+//8 
+function longerThanSeven (array) {
+    return array.some(word => word.length > 7);
+}
+// console.log(longerThanSeven(animals));
+
+//9
+function allFour (array) {
+    return array.every(word => word.length === 4);
+}
+// console.log(allFour(animals));
+
+//10
+function concatStrings (array) {
+    return array.reduce((a, b) => a + ` ${b}`);
+}
+// console.log(concatStrings(animals));
